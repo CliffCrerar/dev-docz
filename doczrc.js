20f88f496de546ab3f7378aa457dbb69400d4200
Cliff Crerar <cliff.crerar@gmail.com>
2019-08-09 10:55:51 +0200

    Hos ja

--- doczrc.js
+++ doczrc.js
@@ -1,5 +1,5 @@
 const chLog = false;
-const path = require( 'path' );
+const path = require('path');
 // import themeDefault from 'docz-theme-default/dist'
 // import { splash } from 'docz-plugin-splash'
 
@@ -7,30 +7,30 @@ const PUBLIC = path.resolve(__dirname, 'public');
 const SRC = path.resolve(__dirname, 'src');
 
 const doczConfig = {
-	hashRouter: true,
-	title: 'My Dev Docz',
-	description: 'This is my awesome documentation',
-	menu: require( SRC+'/menu' ),
-	themeConfig: {
-		mode: 'light'
-		// logo: {
-		//     src: 'https://cdn-learnsql.c1i44.now.sh/pg-wip/_under_construction.png'
-		// }
-	},
-	files: [
-		SRC+'/**/*.{md,markdown,mdx}'
-	],
-	base: "/",
-	public: PUBLIC,
-	indexHtml: 'index.html',
-	dest: "/dist"
-	// modifyBundlerConfig: config => {
-	//     (function() {
-	//         console.log('when this runs');
-	//         //console.log('document', document);
-	//     }())
-	// }
-	//plugins: [splash()]
+    hashRouter: true,
+    title: 'My Dev Docz',
+    description: 'This is my awesome documentation',
+    menu: require(SRC + '/menu'),
+    themeConfig: {
+        mode: 'light'
+            // logo: {
+            //     src: 'https://cdn-learnsql.c1i44.now.sh/pg-wip/_under_construction.png'
+            // }
+    },
+    files: [
+        SRC + '/**/*.{md,markdown,mdx}'
+    ],
+    base: "/",
+    public: PUBLIC,
+    indexHtml: 'index.html',
+    dest: "/dist"
+        // modifyBundlerConfig: config => {
+        //     (function() {
+        //         console.log('when this runs');
+        //         //console.log('document', document);
+        //     }())
+        // }
+        //plugins: [splash()]
 };
 
 // if (chLog) {
